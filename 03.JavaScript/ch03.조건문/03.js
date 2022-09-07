fa86f485a08e54e5733a7badd2ee87e3e441492d
Myungjae Lee <lmj7303@gmail.com>
2022-09-07 17:35:26 +0900

    java

---
+++ 03.JavaScript/ch03.조건문/03.js
@@ -0,0 +1,13 @@
+var jum=Math.ceil(Math.random()*100);
+switch (jum){
+    case jum>=90;
+        console.log('A')
+    case jum>=80;
+        console.log('B')
+    case jum>=70;
+        console.log('c')
+    case jum>=60;
+        console.log('D')
+    case jum<60;
+        sconsole.log('F')
+}
