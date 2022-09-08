a70d5d456462d0ab95358e49591486607dbb758e
Myungjae Lee <lmj7303@gmail.com>
2022-09-08 15:39:04 +0900

    java

---
+++ 03.JavaScript/ch05.함수/01.함수선언.js
@@ -0,0 +1,15 @@
+// 함수 이름을 갖는ㄴ 기본 함수
+function funcName(){
+    console.log('함수 이름을 갖는 함수');
+}
+funcName();
+
+// 익명(Anonymous) 함수
+const fn= function(){
+    console.log('익명 함수');
+}
+fn();
+console.log(fn,typeof fn);
+
+// 화살표 함수()
+const arrowFunc
