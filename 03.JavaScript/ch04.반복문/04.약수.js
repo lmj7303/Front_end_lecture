4031d9d9acc8ff44460d738dac3d45efaf77e1df
Myungjae Lee <lmj7303@gmail.com>
2022-09-08 11:59:35 +0900

    js

---
+++ 03.JavaScript/ch04.반복문/04.약수.js
@@ -0,0 +1,10 @@
+// 10보다 큰 정수를 입력으로 받아 약수를 모두 배열로 만들어서 출력하세요.
+
+let input=12;
+let divisors=[]; //[i for i in range(1,input+1) if input%i==0]
+for (let i=1;i<=input;i++){
+    if (input%i==0)
+    divisors.push(i);
+    //console.log(i);
+}
+console.log(divisors);
