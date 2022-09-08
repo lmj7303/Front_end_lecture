8e7d1625e7a67664a5a489f4e90e6d7ed2e8f925
Myungjae Lee <lmj7303@gmail.com>
2022-09-08 16:43:01 +0900

    java

--- 03.JavaScript/ch05.함수/03.매개변수초기화.js
+++ 03.JavaScript/ch05.함수/03.매개변수초기화.js
@@ -2,7 +2,8 @@
 function print(name,count){
     // if (!count)
     //     count=0;
-    count=count?count:0;
+    count=count?count:0; 
+    // count=count||0;
     console.log(`${name}이/가 ${count}개 있습니다.`);
 }
 
