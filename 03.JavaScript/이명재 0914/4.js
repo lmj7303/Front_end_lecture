cbcc042d8f948bb6d0b5306fa7931dd06cd528d2
Myungjae Lee <lmj7303@gmail.com>
2022-09-14 16:40:36 +0900

    연습

--- 03.JavaScript/이명재 0914/4.js
+++ 03.JavaScript/이명재 0914/4.js
@@ -1,7 +1,6 @@
 // 4. 다음 배열에서 제조사(manufacturer)와 모델명(model)을 분리해서 
 // 별도의 배열을 각각 만드세요.
 
-const cars='';
 const cars = ['buick skylark 320', 'plymouth satellite', 'amc rebel sst', 'ford torino'];
 const words=cars.split(',');
 console.log(words);
