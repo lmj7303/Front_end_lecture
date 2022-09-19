d41df106870e80bcc6b5a5fc2819be258ee873a2
Myungjae Lee <lmj7303@gmail.com>
2022-09-19 10:01:47 +0900

    cc

--- 03.JavaScript/ch14.jQuery/이명재/13.연습문제1.js
+++ 03.JavaScript/ch14.jQuery/이명재/13.연습문제1.js
@@ -1,6 +1,6 @@
 function answer1() {
-    const first = parseInt($('#first').value);
-    const second = parseInt($('#second').value);
+    const first = parseInt($('#first').val());
+    const second = parseInt($('#second').val());
     console.log(first, second);
     let result = [];
     for (let i=first; i<=second; i++) {
@@ -8,11 +8,11 @@ function answer1() {
             continue;
         result.push(Math.pow(2, i));
     }
-    $('result1').html() = result.join(', ');
+    $('#result1').html(result.join(', '));
 }
 
 function answer2() {
-    const text = $('#text').value;
+    const text = $('#text').val();
     const noPunct = text.replace(/[-',.]/g, '').replace(/\n/g, ' ');    // 구둣점 제거, 줄바꿈은 공백으로 변경
     console.log(noPunct);
     const words = noPunct.split(' ');
@@ -21,12 +21,12 @@ function answer2() {
         if (word == 'the')
             count++;
     }
-    $('result2').html() = `the의 갯수는 <strong>${count}</strong>개`;
+    $('#result2').html(`the의 갯수는 <strong>${count}</strong>개`);
 }
 
 function answer3() {
-    const num1 = parseInt($('#num1').value);
-    const num2 = parseInt($('#num2').value);
+    const num1 = parseInt($('#num1').val());
+    const num2 = parseInt($('#num2').val());
     const divisors1 = [];
     for (let i=1; i<=num1; i++) {
         if (num1 % i == 0)
@@ -38,5 +38,35 @@ function answer3() {
             divisors2.push(i);      // num2의 약수
     }
     const commonDivisors = divisors1.filter(val => divisors2.includes(val));
-    $('result3').html() = `Set(${commonDivisors.join(', ')})`;
+    $('#result3').html(`Set(${commonDivisors.join(', ')})`);
+}
+
+let count = 3;
+var show;
+$(document).ready(function() {
+    show = $('#show');
+});
+function disp(num) {
+    console.log(`disp(${num})`);
+    $('<img></img>')
+        .attr('src', '../ch13.문서객체모델/digits/'+num+'.svg')
+        .attr('width', '800')
+        .css({marginTop:'30px', marginBottom:'200px', marginLeft:'200px'})
+        .appendTo(show)
+}
+function hide() {
+    console.log('hide()');
+    show.children().remove();
+}
+function countDown() {
+    console.log('countDown()');
+    disp(count--);
+    let interval = setInterval(() => {
+        hide();
+        if (count != 0) 
+            disp(count--);
+    }, 1000);
+    setTimeout(() => {
+        clearInterval(interval);
+    }, 3000);
 }
